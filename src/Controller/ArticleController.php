0512acb9a180ad5b616e2ad7737e9053ea2527ff
alv <adlv@test.com>
2023-11-01 10:41:09 +0100

    nelmio cors bundle for Cross-origin resource sharing

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
 use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -12,11 +13,19 @@ use Symfony\Component\Serializer\SerializerInterface;
 class ArticleController extends AbstractController
 {
 
-    #[Route('/api/articles', name: 'article', methods: ['GET'])]
+    #[Route('/api/articles', name: 'articles', methods: ['GET'])]
     public function getAllArticles(ArticleRepository $articleRepository, SerializerInterface $serializer): JsonResponse
     {
         $articles = $articleRepository->findAll();
         $jsonArticles = $serializer->serialize($articles, 'json');
         return new JsonResponse($jsonArticles, Response::HTTP_OK, [], true);
+    }
+
+    #[Route('/api/articles/{id}', name: 'article', methods: ['GET'])]
+    public function getArticle(Article $articleUrl, ArticleRepository $articleRepository, SerializerInterface $serializer): JsonResponse
+    {
+        $article = $articleRepository->find($articleUrl);
+        $jsonArticle = $serializer->serialize($article, 'json');
+        return new JsonResponse($jsonArticle, Response::HTTP_OK, [], true);
     }
 }
