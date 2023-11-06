3c511b9df0109fff15d7ce1a84a58722b2e9f1b4
alv <adlv@test.com>
2023-11-06 17:39:58 +0100

    UserController, CRUD

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,18 +2,77 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGenerator;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class UserController extends AbstractController
 {
-    #[Route('/api/users', name: 'users', methods: 'GET')]
-    public function getAllUsers(): JsonResponse
+    #[Route('/api/users', name: 'users', methods: ['GET'])]
+    public function getAllUsers(UserRepository $userRepository, SerializerInterface $serializer): JsonResponse
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/UserController.php',
-        ]);
+        $users = $userRepository->findAll();
+        $jsonUsers = $serializer->serialize($users, 'json', ["groups" => "getUser"]);
+        return new JsonResponse($jsonUsers, Response::HTTP_OK, [], true);
+    }
+
+    #[Route('/api/users/{id}', name: 'detailUser', methods: ['GET'])]
+    public function getOneUser(User $user, SerializerInterface $serializer): JsonResponse
+    {
+        $jsonUser = $serializer->serialize($user, 'json', ["groups" => "getUser"]);
+        return new JsonResponse($jsonUser, Response::HTTP_OK, [], true);
+    }
+
+    #[Route('/api/users/{id}', name: 'deleteUser', methods: ['DELETE'])]
+    public function deleteUser(User $user, EntityManagerInterface $em): JsonResponse
+    {
+        $em->remove($user);
+        $em->flush();
+        return new JsonResponse(null, Response::HTTP_NO_CONTENT);
+    }
+
+    #[Route('/api/users', name: 'createUser', methods: 'POST')]
+    public function createUser(Request $request, SerializerInterface $serializer, EntityManagerInterface $em, UrlGenerator $urlGenerator, ValidatorInterface $validator)
+    {
+        $user = $serializer->deserialize($request->getContent(), User::class, 'json');
+        $errors = $validator->validate($user);
+        if ($errors->count() > 0) {
+            throw new HttpException(Response::HTTP_BAD_REQUEST, "La requête n'est pas valide");
+        }
+
+        $em->persist($user);
+        $em->flush();
+
+        $jsonUser = $serializer->serialize($user, 'json', ["groups" => "getUser"]);
+        $location = $urlGenerator->generate('detailUser', ['id' => $user->getId()], UrlGeneratorInterface::ABSOLUTE_URL);
+        return new JsonResponse($jsonUser, Response::HTTP_CREATED, ['location' => $location], true);
+
+    }
+
+    #[Route('/api/users', name: 'updateUser', methods: 'PUT')]
+    public function updateUser(Request $request, SerializerInterface $serializer, EntityManagerInterface $em, User $currentUser, ValidatorInterface $validator)
+    {
+        $user = $serializer->deserialize($request->getContent(), User::class, 'json', [AbstractNormalizer::OBJECT_TO_POPULATE => $currentUser]);
+        $errors = $validator->validate($user);
+        if ($errors->count() > 0) {
+            throw new HttpException(Response::HTTP_BAD_REQUEST, "La requête n'est pas valide");
+        }
+
+        $em->persist($user);
+        $em->flush();
+
+        return new JsonResponse(null, Response::HTTP_NO_CONTENT);
     }
 }
